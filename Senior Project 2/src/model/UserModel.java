d0d25529700c2371aaed53a3d39f370dbed9ef95
Anna Blasingame <amblas5687@ung.edu>
2018-02-02 17:31:07 -0500

    changed inport error

--- Senior Project 2/src/model/UserModel.java
+++ Senior Project 2/src/model/UserModel.java
@@ -1,12 +1,5 @@
 package model;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-
 public class UserModel {
 	
 	    private String fname;
