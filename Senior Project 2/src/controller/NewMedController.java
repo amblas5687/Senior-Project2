615ce33f6146cb440299d706d1a7717b66ebd791
Anna Blasingame <amblas5687@ung.edu>
2018-02-20 12:08:51 -0500

    Preliminary styling for GUIs. Larger text and scene.

--- Senior Project 2/src/controller/NewMedController.java
+++ Senior Project 2/src/controller/NewMedController.java
@@ -3,7 +3,6 @@ package controller;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 import application.AnnaMain;
 import application.DBConfig;
 import javafx.event.ActionEvent;
