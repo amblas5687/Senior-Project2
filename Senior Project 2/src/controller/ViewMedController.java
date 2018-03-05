beb994f82f71b8c881c77a8f7c100301c652daa4
danni <danni@JIN.attlocal.net>
2018-03-04 19:50:58 -0500

    Setup for changing between current and archived medication views.

--- Senior Project 2/src/controller/ViewMedController.java
+++ Senior Project 2/src/controller/ViewMedController.java
@@ -6,19 +6,59 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 
 public class ViewMedController {
 
 	@FXML
 	private Button add;
+	
+    @FXML
+    private ToggleButton currMed;
+
+    @FXML
+    private ToggleButton archMed;
+
+	@FXML
+	private AnchorPane table_med = new AnchorPane();
 
 	@FXML
 	private AnchorPane content_view;
 
 	private URL toPane;
 	private AnchorPane temp;
+	
+	private ToggleGroup state = new ToggleGroup();
+	
+	public void initialize(){
+	 try {
+		 
+		 toPane = getClass().getResource("/view/CurrentMedsView.fxml"); 
+  		 temp = FXMLLoader.load(toPane);
+		 table_med.getChildren().setAll(temp);
+		 
+	 } catch(Exception e) {
+		 e.printStackTrace();
+	 }
 	 
+	 //add to toggle group
+	 currMed.setToggleGroup(state);
+	 archMed.setToggleGroup(state);
+		 
+	}
+	
+	@FXML
+	private void currentMed(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	private void archivedMed(ActionEvent event) {
+		
+	}
+	
 	@FXML
 	private void addMed(ActionEvent event) {
 		
