5aac3556f34115c60340289a4617e6e82ef11d37
Danni <dhsmit6900@ung.edu>
2018-03-12 16:50:21 -0400

    Basic functionality. No error checking.

--- Senior Project 2/src/controller/LoginController.java
+++ Senior Project 2/src/controller/LoginController.java
@@ -1,15 +1,29 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import application.AnnaMain;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
+	
+	 Connection conn = AnnaMain.con;
 
+	 Parent root;
+	 Scene scene;
+	 Stage stage;
+	 
 	 @FXML
-	 private TextField usernameTF;
+	 private TextField emailTF;
 
 	 @FXML
 	 private PasswordField passwordTF;
@@ -19,6 +33,35 @@ public class LoginController {
 
 	 @FXML
 	 void login(ActionEvent event) {
+		 
+		 try {
+			 String email = emailTF.getText();
+			 String password = passwordTF.getText();
+			 String query = "SELECT * FROM user WHERE email = ? AND password = ?";
+			 
+			 PreparedStatement ps = conn.prepareStatement(query);
+			 ps.setString(1, email);
+			 ps.setString(2, password);
+			 ResultSet rs = ps.executeQuery();
+			 
+			 while(rs.next()){
 
+					String mail = rs.getString("email");
+					String pass = rs.getString("password");
+			 
+					 if(mail.equals(email) && pass.equals(password)){
+							stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+							root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
+							scene = new Scene(root);
+							stage.setScene(scene);
+					 } else {
+						 System.out.println("Unsuccessful login");
+					 }
+			 }
+			 
+			 
+		 }catch(Exception e) {
+			 e.printStackTrace();
+		 }
 	 }
 }
