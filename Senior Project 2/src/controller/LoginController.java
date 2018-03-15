f3e9fb4fe9dd510fa867f2a7db5fa5222cf830ee
Danni <dhsmit6900@ung.edu>
2018-03-15 15:49:51 -0400

    Error checking done for login.

--- Senior Project 2/src/controller/LoginController.java
+++ Senior Project 2/src/controller/LoginController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -36,6 +37,9 @@ public class LoginController {
 
 	 @FXML
 	 private JFXButton btnPatient;
+	 
+	 @FXML 
+	 private Label lblLogin;
 	 
 	 
 	//global variables for login
@@ -79,81 +83,89 @@ public class LoginController {
 	 @FXML
 	 void login(ActionEvent event) {
 		 
-		 	Connection connection = null;
-	    	PreparedStatement ps = null;
-	    	ResultSet rs = null;
+		 String email = emailTF.getText();
+		 String password = passwordTF.getText();
 		 
-		 try {
+		 lblLogin.setText(null);
+		 
+		 if(email.equals("") || password.equals("")) {
+			 lblLogin.setText("Please fill both fields.");
+		 } else {
+			 Connection connection = null;
+		     PreparedStatement ps = null;
+		     ResultSet rs = null;
 			 
-			 connection = DataSource.getInstance().getConnection();
-
-			 String email = emailTF.getText();
-			 String password = passwordTF.getText();
-			 String query = "SELECT * FROM user WHERE email = ? AND password = ?";
-			 
-			 ps = connection.prepareStatement(query);
-			 ps.setString(1, email);
-			 ps.setString(2, password);
-			 rs = ps.executeQuery();
-			 
-			 while(rs.next()){
-
-					String mail = rs.getString("email");
-					String pass = rs.getString("password");
-			 
-					 if(mail.equals(email) && pass.equals(password)){
-						stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-						root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
-						scene = new Scene(root);
-						stage.setScene(scene);
-						
-						currentUserID = rs.getString("userID");
-						currentPatientID = rs.getString("patientCode");
-						
-						System.out.println("CURRENT USER AND PATIENT... " + currentUserID + "__" + currentPatientID);
-						 
-					 } else {
-						 System.out.println("FAILED LOGIN");
-					 }
-			 }
-			 
-			 
-		 }catch (Exception e)
-	    	{
-	    		e.printStackTrace();
-	    	}
-		 	//close connections
-	    	finally {
-				if(connection!=null)
-				{
-					try {
-						connection.close();
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+			 try {
+				 
+				 connection = DataSource.getInstance().getConnection();
+				 
+				 String query = "SELECT * FROM user WHERE email = ? AND password = ?";
+				 
+				 ps = connection.prepareStatement(query);
+				 ps.setString(1, email);
+				 ps.setString(2, password);
+				 rs = ps.executeQuery();
+				 
+				 while(rs.next()){
+	
+						String mail = rs.getString("email");
+						String pass = rs.getString("password");
+				 
+						 if(mail.equals(email) && pass.equals(password)){
+							stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+							root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
+							scene = new Scene(root);
+							stage.setScene(scene);
+							
+							currentUserID = rs.getString("userID");
+							currentPatientID = rs.getString("patientCode");
+							
+							System.out.println("CURRENT USER AND PATIENT... " + currentUserID + "__" + currentPatientID);
+							 
+						 } else {
+							 System.out.println("FAILED LOGIN");
+						 }
+				 }
+				 
+				 lblLogin.setText("Incorrect email or password.");
+				 
+			 }catch (Exception e)
+		    	{
+		    		e.printStackTrace();
+		    	}
+			 	//close connections
+		    	finally {
+					if(connection!=null)
+					{
+						try {
+							connection.close();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 					}
-				}
-				
-				if(ps!=null)
-				{
-					try {
-						ps.close();
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+					
+					if(ps!=null)
+					{
+						try {
+							ps.close();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 					}
-				}
-				
-				if(rs!=null)
-				{
-					try {
-						rs.close();
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+					
+					if(rs!=null)
+					{
+						try {
+							rs.close();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 					}
-				}
-			}//end finally
+				}//end finally
+		  }//end else
 	 }//end login
 	 
 	 
